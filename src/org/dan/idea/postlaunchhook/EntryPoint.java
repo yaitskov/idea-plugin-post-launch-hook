e533839e40db67c1f2033fccffb855efdba07ff4
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-25 14:47:59 +0200

    description

--- src/org/dan/idea/postlaunchhook/EntryPoint.java
+++ src/org/dan/idea/postlaunchhook/EntryPoint.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.stream.Stream;
 
 import com.intellij.openapi.actionSystem.AnAction;
